2b89386c02c0983ace7665e979e40dcea929ea2d
ErikHK <erikhk@gmail.com>
2026-04-13 14:33:47 +0200

    continuous adc woop

--- esp32/i2s_std/main/i2s_std_example_main.c
+++ esp32/i2s_std/main/i2s_std_example_main.c
@@ -1,8 +1,13 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+
+#include "esp_log.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
+
 #include "driver/i2s_std.h"
 #include "driver/gpio.h"
 #include "esp_check.h"
@@ -10,7 +15,9 @@
 #include "i2s_example_pins.h"
 #include <math.h>
 
-#include <esp_adc/adc_oneshot.h>
+//#include <esp_adc/adc_oneshot.h>
+#include "esp_adc/adc_continuous.h"
+
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
 
@@ -37,16 +44,15 @@ static uint16_t i2 = 0;
 static uint16_t i3 = 0;
 
 
-static int16_t vol = 0;
-static int16_t targetvol = 0;
+static int16_t vol = 20;
+static int16_t targetvol = 20;
 static uint8_t volcounter = 0;
-static int16_t volsum = 0;
-
+static int32_t volsum = 0;
 
 static uint16_t sweepvol = 65535>>1;
 static int16_t freq = 163;
 static int16_t freqoffset = 0;
-static int16_t mode = 0;
+static int16_t mode = 2;
 static int16_t ontime = 30000;
 
 static int32_t freqsum = 0;
@@ -57,7 +63,7 @@ static uint16_t tremoloindex = 0;
 static int16_t tremolofreq = 10;
 
 static float freq_q = 0;
-static float pluss = 0;
+static float pluss = 200;
 
 static uint8_t alt_pressed = 0;
 static uint8_t ctrl_pressed = 0;
@@ -70,9 +76,24 @@ static uint32_t phaseAcc = 0;
 #define ADC_BITWIDTH  ADC_BITWIDTH_12   // 12-bit resolution (0-4095)
 #define ADC_ATTEN     ADC_ATTEN_DB_12   // ~3.3V full-scale voltage
 
+
+#define EXAMPLE_READ_LEN 16
+uint8_t result[EXAMPLE_READ_LEN] = {0};
+
+
+
+
 static int adc_value = 0;
 
-adc_oneshot_unit_handle_t adc_handle;
+adc_continuous_handle_t handle = NULL;
+
+
+static adc_channel_t channel[5] = {ADC_CHANNEL_0, ADC_CHANNEL_1, ADC_CHANNEL_2, ADC_CHANNEL_3, ADC_CHANNEL_4};
+
+//static adc_channel_t channel[1] = {ADC_CHANNEL_0};
+
+static TaskHandle_t s_task_handle;
+static const char *TAG = "EXAMPLE";
 
 
 /* Set 1 to allocate rx & tx channels in duplex mode on a same I2S controller, they will share the BCLK and WS signal
@@ -86,6 +107,54 @@ adc_oneshot_unit_handle_t adc_handle;
 #define EXAMPLE_STD_DIN_IO1         GPIO_NUM_22     // I2S data in io number
 
 static i2s_chan_handle_t                tx_chan;        // I2S tx channel handler
+
+
+
+static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
+{
+    BaseType_t mustYield = pdFALSE;
+    //Notify that ADC continuous driver has done enough number of conversions
+    vTaskNotifyGiveFromISR(s_task_handle, &mustYield);
+
+    return (mustYield == pdTRUE);
+}
+
+static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc_continuous_handle_t *out_handle)
+{
+
+    adc_continuous_handle_cfg_t adc_config = {
+        .max_store_buf_size = 1024,
+        .conv_frame_size = 256,
+    };
+    ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
+
+    adc_continuous_config_t dig_cfg = {
+        .sample_freq_hz = 10 * 1000,
+        .conv_mode = ADC_CONV_SINGLE_UNIT_1,
+        .format = ADC_DIGI_OUTPUT_FORMAT_TYPE1
+    };
+
+    adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX] = {0};
+    dig_cfg.pattern_num = channel_num;
+    for (int i = 0; i < channel_num; i++) {
+        adc_pattern[i].atten = ADC_ATTEN_DB_12;
+        adc_pattern[i].channel = channel[i] & 0x7;
+        adc_pattern[i].unit = ADC_UNIT;
+        adc_pattern[i].bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
+
+        ESP_LOGI(TAG, "adc_pattern[%d].atten is :%"PRIx8, i, adc_pattern[i].atten);
+        ESP_LOGI(TAG, "adc_pattern[%d].channel is :%"PRIx8, i, adc_pattern[i].channel);
+        ESP_LOGI(TAG, "adc_pattern[%d].unit is :%"PRIx8, i, adc_pattern[i].unit);
+    }
+    dig_cfg.adc_pattern = adc_pattern;
+    ESP_ERROR_CHECK(adc_continuous_config(handle, &dig_cfg));
+
+    *out_handle = handle;
+}
+
+
+
+
 
 static void write_sound()
 {
@@ -125,7 +194,7 @@ static void write_sound()
             i += (uint16_t) (pluss);
             i2 += (uint16_t) (pluss*1.5);
 
-            tmp = tmp >> 11;
+            tmp = tmp >> 10;
         }
         //soft square
         else if(mode < 4)
@@ -136,7 +205,7 @@ static void write_sound()
             i += (uint16_t) (pluss);
             //i += 200;
 
-            tmp = tmp >> 11;
+            tmp = tmp >> 7;
 
             //tremoloindex += 1;
         }
@@ -226,20 +295,112 @@ static void tremolo_task(void *userData)
 
 void read_buttons(void *userData)
 {
+
+    
+    uint32_t ret_num = 0;
+    esp_err_t ret = adc_continuous_read(handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
+    if (ret == ESP_OK) {
+        //ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32" bytes", ret, ret_num);
+
+        adc_continuous_data_t parsed_data[ret_num / SOC_ADC_DIGI_RESULT_BYTES];
+        uint32_t num_parsed_samples = 0;
+
+        esp_err_t parse_ret = adc_continuous_parse_data(handle, result, ret_num, parsed_data, &num_parsed_samples);
+        if (parse_ret == ESP_OK) {
+            for (int i = 0; i < num_parsed_samples; i++) {
+                if (parsed_data[i].valid) {
+                    //ESP_LOGI(TAG, "ADC%d, Channel: %d, Value: %"PRIu32,
+                    //            parsed_data[i].unit + 1,
+                    //            parsed_data[i].channel,
+                    //            parsed_data[i].raw_data);
+
+                    if(parsed_data[i].channel == 0)
+                    {
+                        int16_t tmpfreq = parsed_data[i].raw_data;
+
+                        if(tmpfreq > 60)
+                        {
+                            //freqsum += tmpfreq;
+                            freq = tmpfreq;
+                        }
+                    }
+
+                    if(parsed_data[i].channel == 1)
+                    {
+                        int16_t tmpvol = (parsed_data[i].raw_data - 760);
+
+                        if(tmpvol < 0)
+                            tmpvol = 0;
+
+                        if(tmpvol >= 2047)
+                            tmpvol = 2047;
+                        
+
+                        volsum += tmpvol;
+
+                        //volcounter++;
+
+                        //if(volcounter == 15)
+                        //{
+                        //    targetvol = volsum >> 4;
+                        //    volcounter = 0;
+                        //    volsum = 0;
+                        //}
+
+                    }
+                    
+
+                } else {
+                    ESP_LOGW(TAG, "Invalid data [ADC%d_Ch%d_%"PRIu32"]",
+                                parsed_data[i].unit + 1,
+                                parsed_data[i].channel,
+                                parsed_data[i].raw_data);
+                }
+            }
+        }
+
+    }
+
+    //now volsum has 16 samples!
+    targetvol = volsum>>4;
+    //reset volsum
+    volsum = 0;
+
+    //same with freq!
+    //freq = freqsum>>4;
+    //freqsum = 0;
+
+    int16_t freqoffsetq = freqoffset/100;
+
+    float freqoffsetf = freqoffsetq / 12.0f;
+
+    freq_q = (float) (freq + 100) / 1860.0f;
+
+    pluss = 45 * pow(2.0f, freq_q + freqoffsetf);
+
+
+
+
     //while(1)
     //{
-        if(gpio_get_level(CTRL_BUTTON_PIN))
-            ctrl_pressed = 1;
-        else
-            ctrl_pressed = 0;
+    if(gpio_get_level(CTRL_BUTTON_PIN))
+        ctrl_pressed = 1;
+    else
+        ctrl_pressed = 0;
 
 
-        if(gpio_get_level(ALT_BUTTON_PIN))
-            alt_pressed = 1;
-        else
-            alt_pressed = 0;
+    if(gpio_get_level(ALT_BUTTON_PIN))
+        alt_pressed = 1;
+    else
+        alt_pressed = 0;
+
+
+
 
         
+        
+
+        /*
         //min 750 max 2800!
         adc_oneshot_read(adc_handle, ADC_CHANNEL_1, &adc_value);
 
@@ -248,6 +409,8 @@ void read_buttons(void *userData)
 
         int16_t tmpvol = (adc_value - 740);
 
+
+
         if(tmpvol < 0)
             tmpvol = 0;
 
@@ -255,7 +418,7 @@ void read_buttons(void *userData)
             tmpvol = 2047;
         
 
-        volsum += tmpvol;
+        volsum += tmpvol>>2;
 
         volcounter++;
 
@@ -265,6 +428,11 @@ void read_buttons(void *userData)
             volcounter = 0;
             volsum = 0;
         }
+
+        //targetvol = 500;
+        //vol = 500;
+
+        //printf("%d\n", targetvol);
 
         
         adc_oneshot_read(adc_handle, ADC_CHANNEL_0, &adc_value);
@@ -315,18 +483,35 @@ void read_buttons(void *userData)
         if(ctrl_pressed)
             ontime = adc_value << 3;
 
-        printf("ontime %d\n", ontime);
+        //printf("ontime %d\n", ontime);
         
         //vTaskDelay(pdMS_TO_TICKS(10)); // Delay ms
     //}
+
+    */
 }
 
+
+#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
+#define EXAMPLE_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE1
+#define EXAMPLE_ADC_GET_CHANNEL(p_data)     ((p_data)->type1.channel)
+#define EXAMPLE_ADC_GET_DATA(p_data)        ((p_data)->type1.data)
+#else
+#define EXAMPLE_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE2
+#define EXAMPLE_ADC_GET_CHANNEL(p_data)     ((p_data)->type2.channel)
+#define EXAMPLE_ADC_GET_DATA(p_data)        ((p_data)->type2.data)
+#endif
 
 
 static void audio_task(void *userData)
 {
     
     while(1) {
+
+        
+
+
+        //vTaskDelay(1);
         write_sound();
 
 
@@ -342,15 +527,69 @@ void timer_callback(void *param)
     //targetvol += 100;
 
     if(targetvol > vol)
-            vol+=25;
+            vol+=4;
         else 
-            vol-=25;
+            vol-=4;
 }
-
 
 
 void app_main(void)
 {
+
+    //ADC
+
+    esp_err_t ret;
+    uint32_t ret_num = 0;
+    uint8_t result[EXAMPLE_READ_LEN] = {0};
+    memset(result, 0xcc, EXAMPLE_READ_LEN);
+
+    s_task_handle = xTaskGetCurrentTaskHandle();
+
+    adc_continuous_handle_t handle = NULL;
+
+    continuous_adc_init(channel, sizeof(channel) / sizeof(adc_channel_t), &handle);
+
+    adc_continuous_evt_cbs_t cbs = {
+        .on_conv_done = s_conv_done_cb,
+    };
+
+    ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));
+    ESP_ERROR_CHECK(adc_continuous_start(handle));
+
+
+/*
+    while(1){
+
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        while(1)
+        {
+
+        ret = adc_continuous_read(handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
+        if (ret == ESP_OK) {
+            ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32" bytes", ret, ret_num);
+
+            for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
+                adc_digi_output_data_t *p = (adc_digi_output_data_t*)&result[i];
+                uint32_t chan_num = EXAMPLE_ADC_GET_CHANNEL(p);
+                uint32_t data = EXAMPLE_ADC_GET_DATA(p);
+                // Check the channel number validation, the data is invalid if the channel num exceed the maximum channel 
+                if (chan_num < SOC_ADC_CHANNEL_NUM(ADC_UNIT)) {
+                    //ESP_LOGI(TAG, "Unit: %s, Channel: %"PRIu32", Value: %"PRIx32, unit, chan_num, data);
+                    //ESP_LOGI(TAG, "Unit: %s, Channel: %"PRIu32", Value: %lu", chan_num, data);
+                    ESP_LOGI(TAG, "Channel: %d, Value: %lu", chan_num, data);
+                } else {
+                    ESP_LOGW(TAG, "Invalid data [_%"PRIu32"_%"PRIx32"]", chan_num, data);
+                }
+            }
+
+
+            vTaskDelay(1);
+
+        }
+    }
+    }
+*/
 
 
     //button read timer
@@ -363,9 +602,7 @@ void app_main(void)
 
     esp_timer_handle_t timer_handler2;
     esp_timer_create(&my_timer_args2, &timer_handler2);
-    esp_timer_start_periodic(timer_handler2, 5000);  // One Second = 1000000 micro second
-
-
+    esp_timer_start_periodic(timer_handler2, 2000);  // One Second = 1000000 micro second
 
 
     const esp_timer_create_args_t my_timer_args = 
@@ -376,10 +613,10 @@ void app_main(void)
 
     esp_timer_handle_t timer_handler;
     esp_timer_create(&my_timer_args, &timer_handler);
-    esp_timer_start_periodic(timer_handler, 1000);  // One Second = 1000000 micro second
+    esp_timer_start_periodic(timer_handler, 10000);  // One Second = 1000000 micro second
 
 
-
+  /*
     // Initialize ADC Oneshot Mode Driver on the ADC Unit
     adc_oneshot_unit_init_cfg_t init_config = {
         .unit_id = ADC_UNIT,
@@ -404,6 +641,7 @@ void app_main(void)
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_3, &config));
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_4, &config));
 
+    */
 
     i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
     ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, &tx_chan, NULL));
@@ -457,7 +695,9 @@ void app_main(void)
 
 
     //xTaskCreate(audio_task, "audio", 4096, NULL, configMAX_PRIORITIES - 1, NULL);
+    
     xTaskCreate(audio_task, "audio", 4096, NULL, 2 | portPRIVILEGE_BIT, NULL);
+    
     //xTaskCreate(read_buttons, "read_buttons", 4096, NULL, 5, NULL);
     //xTaskCreate(tremolo_task, "tremolo", 4096, NULL, 5, NULL);
     //xTaskCreate(vol_envelope, "volume", 4096, NULL, ( 1 | portPRIVILEGE_BIT ), NULL);
